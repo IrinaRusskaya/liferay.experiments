56637b30bd8afc2c3e7eef6c52972be1c06c54c8
Irina <russk-irina@gmail.com>
2016-03-19 18:32:29 +0100

    new functionality

--- test-portlet/docroot/WEB-INF/src/com/test/liferay/TestPortlet.java
+++ test-portlet/docroot/WEB-INF/src/com/test/liferay/TestPortlet.java
@@ -1,11 +1,16 @@
 package com.test.liferay;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
 /**
  * Portlet implementation class TestPortlet
  */
 public class TestPortlet extends MVCPortlet {
- 
+	public void addEntry(ActionRequest request, ActionResponse response) {
+
+	}
 
 }
