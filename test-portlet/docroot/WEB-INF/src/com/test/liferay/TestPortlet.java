f9d4a35b3e6cc2e133cda9832e91157bf43f49fa
Irina <russk-irina@gmail.com>
2016-03-19 18:22:44 +0100

    test-portlet created

---
+++ test-portlet/docroot/WEB-INF/src/com/test/liferay/TestPortlet.java
@@ -0,0 +1,11 @@
+package com.test.liferay;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class TestPortlet
+ */
+public class TestPortlet extends MVCPortlet {
+ 
+
+}
